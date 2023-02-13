7981afa9fced063e8867f3e4536fc2c255b2d0a6
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-02-13 21:44:12 +0000

    drag and drop API

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -20,7 +20,8 @@ let onHoldListArray = []
 let listArrays = [];
 
 // Drag Functionality
-
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -58,6 +59,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart', 'drag(event)');
   // Append
   columnEl.appendChild(listEl);
 }
@@ -87,6 +90,32 @@ function updateDOM() {
   // Run getSavedColumns only once, Update Local Storage
 
 
+}
+
+// When Item Starts Dragging
+const drag = e => {
+  draggedItem = e.target;
+}
+
+// Column Allows for Item to Drop
+const allowDrop = e => e.preventDefault();
+
+// When Item Enters Column Area
+const dragEnter = column => {
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+};
+
+// Dropping Item in Column
+const drop = e => {
+  e.preventDefault();
+
+  // Remove Background Color/Padding
+  listColumns.forEach(column => column.classList.remove('over'));
+
+  // Add Item to Column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 // On Load
